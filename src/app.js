abf89a795114de31609a1759449d639acb9fdd07
dongyuanxin <yuanxin.me@gmail.com>
2019-07-16 13:12:41 +0800

    add: 用户登录，初始化全局用户信息

--- src/app.js
+++ src/app.js
@@ -10,26 +10,100 @@ App({
       })
     }
 
-    this.globalData = {}
+    this.globalData = {
+      user: {}
+    }
 
+    this.login()
+  },
+
+  // 登录并且拿到openid
+  login () {
+    const that = this
     wx.login({
       success(res) {
         console.log(res.code)
-        // wx.request({
-        //   url: 'https://api.weixin.qq.com/sns/jscode2session',
-        //   data: {
-        //     appid: 'wxe23cbe40231fcfe5',
-        //     secret: '8b8269317bc3d1182f64148ead4419cc',
-        //     js_code: res.code,
-        //     grant_type: 'authorization_code'
-        //   },
-        //   method: 'GET',
-        //   success(wxRes) {
-        //     console.log(wxRes)
-        //   }
-        // })      
+        wx.cloud
+          .callFunction({
+            name: 'openid',
+            data: {
+              code: res.code
+            }
+          })
+          .then(res => {
+            const { result } = res
+            if (result.code === 0) {
+              that.globalData.user.openid = result.openid
+              that.getUserInfo()
+            } else {
+              wx.showToast({
+                title: '获取openid失败',
+                icon: 'error',
+                duration: 20000
+              })
+            }
+          })
+          .catch(console.error)
       }
     })
   },
+
+  async getHistoryUserInfo () {
+    const that = this
+    const db = wx.cloud.database({
+      env: 'firsttest-qee47'
+    })
+
+    try {
+      const res = await db.collection('users')
+        .where({
+          openid: that.globalData.user.openid
+        })
+        .get()
+      if (res.data.length) {
+        const user = res.data[0]
+        that.globalData.user.avatarUrl = user.avatarUrl
+        that.globalData.user.nickName = user.nickName
+        return true
+      }
+    } catch (error) {
+      console.error(error)
+    }
+
+    return false
+  },
+
+  async getUserInfo () {
+    const that = this
+    const hasLogin = await that.getHistoryUserInfo()
+    if (hasLogin) {
+      console.log('此用户之前登录过')
+      return
+    }
+
+    wx.getUserInfo({
+      success (res) {
+        const { nickName, avatarUrl } = res.userInfo
+        that.globalData.user.nickName = nickName
+        that.globalData.user.avatarUrl = avatarUrl
+
+        const db = wx.cloud.database({
+          env: 'firsttest-qee47'
+        })
+        db.collection('users')
+          .add({
+            data: {
+              openid: that.globalData.user.openid,
+              nickName,
+              avatarUrl
+            }
+          })
+          .then(res => {
+            console.log(res)
+          })
+          .catch(console.error) 
+      }
+    })
+  }
 
 })
